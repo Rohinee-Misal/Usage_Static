24eabca9e7662c31889a53b8f313820b7b39acae
Rohinee-Misal <rohineemisal92@gmail.om>
2019-02-28 19:15:09 +0530

    Initial commit

---
+++ app/src/main/java/com/rohinee/ganesh/usage_static/MainActivity.java
@@ -0,0 +1,18 @@
+package com.rohinee.ganesh.usage_static;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_app_usage_statistics);
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.container, AppUsageStatisticsFragment.newInstance())
+                    .commit();
+        }
+    }
+}
